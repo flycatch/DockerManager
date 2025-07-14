8ee5a0f71647f76365d2c587075b99740e591a68
akhil-flycatch <akhil.v@flycatchtech.com>
2025-07-14 09:40:09 +0530

    fix(logs screen): log screen issue fix

--- service.py
+++ service.py
@@ -1,27 +1,29 @@
 import requests_unixsocket
 
+
 DOCKER_SOCKET_URL = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
 session = requests_unixsocket.Session()
 
 
-def get_containers() -> list[tuple[str, str, str, str]]:
+def get_containers() -> list[tuple[int ,str, str, str, str]]:
     response = session.get(f"{DOCKER_SOCKET_URL}/containers/json", params={"all": "1"})
 
     if response.status_code != 200:
-        return [("N/A", "Error", "N/A", f"HTTP {response.status_code}")]
+        return [(0, "N/A", "Error", "N/A", f"HTTP {response.status_code}")]
 
     data = response.json()
     if not data:
-        return [("N/A", "No containers", "", "")]
+        return [(0,"N/A", "No containers", "", "")]
 
     return [
         (
+            idx + 1,
             container["Id"][:12],
             container["Names"][0].strip("/"),
             container["Image"],
             container["Status"],
         )
-        for container in data
+        for idx, container in enumerate(data)
     ]
 
 
