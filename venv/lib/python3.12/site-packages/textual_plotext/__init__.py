8c9363bd5ba81b8b61d65dedf6491125a74e2200
akhil <nandakishorep212@gmail.com>
2025-09-23 16:26:08 +0530

    [FIX] removed venv

--- venv/lib/python3.12/site-packages/textual_plotext/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A Textual widget library for wrapping the Plotext terminal plotting library."""
-
-from .plot import Plot, themes
-from .plotext_plot import PlotextPlot
-
-__all__ = ["Plot", "PlotextPlot", "themes"]
